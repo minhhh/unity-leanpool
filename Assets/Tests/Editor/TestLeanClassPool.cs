f488d8edda5b96617f6c6c858b083b48d58e6d1b
Ha Huy Minh <minh.ha@dena.com>
2017-02-21 00:47:12 +0700

    initial commit

---
+++ Assets/Tests/Editor/TestLeanClassPool.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+using Lean;
+
+public class TestLeanClassPool
+{
+    [Test]
+    public void CanNotSpawnWhenEmpty ()
+    {
+        LeanClassPool <Object>.Clear ();
+        Assert.IsNull (LeanClassPool <Object>.Spawn ());
+    }
+
+    [Test]
+    public void CanSpawnLastObject ()
+    {
+        LeanClassPool <Object>.Clear ();
+        var o = new Object ();
+        LeanClassPool <Object>.Despawn (o);
+
+        Assert.AreSame (LeanClassPool <Object>.Spawn (), o);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,16 @@
+# Lean Pool
+
+Lean Pool is a GameObject pooling library that allows you to easily increase the performance of your games.
+
+The original Asset from AssetStore is [here](https://www.assetstore.unity3d.com/en/#!/content/35666)
+
+To include Lean Pool into your project, you can use npm method of unity package manament described [here](https://github.com/minhhh/UBootstrap).
+
+## Changelog
+
+**0.0.1**
+
+* Initial commit
+
+<br/>
+
